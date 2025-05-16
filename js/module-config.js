d74479a3d9005372f8169dc23bc267e1a2c2afea
AtmoOmen <110901588+AtmoOmen@users.noreply.github.com>
2025-05-16 14:52:35 +0800

    1.5.9.0

--- js/module-config.js
+++ js/module-config.js
@@ -9,29 +9,32 @@ const ModuleConfig = {
             6: '界面操作',
             7: '金碟',
             8: '通知',
-            9: '辅助'
+            9: '辅助',
+            10: '脚本'
         },
         en: {
             1: 'General',
             2: 'System',
-            3: 'Skills',
+            3: 'Action',
             4: 'Combat',
             5: 'UI Optimization',
             6: 'UI Operation',
             7: 'Gold Saucer',
-            8: 'Notifications',
-            9: 'Assistants'
+            8: 'Notification',
+            9: 'Assist',
+            10: 'Script'
         },
         ja: {
             1: '一般',
             2: 'システム',
             3: 'スキル',
             4: '戦闘',
-            5: 'UI最適化',
-            6: 'UI操作',
+            5: 'UI 最適化',
+            6: 'UI 操作',
             7: 'ゴールドソーサー',
             8: '通知',
-            9: 'アシスタント'
+            9: 'アシスタント',
+            10: 'スクリプト'
         }
     },
     permissionTypes: {
